b86c5bf8840f1e1ae03e40a8d72ca6de9b3f3c94
John Bretherton <270022735@bun2-206-10.local>
2019-05-30 11:56:16 +1200

    Struggling to get a slideshow started

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// $('slideContainer').each(function(){
+//     if(class === 'active'){
+//         class !== 'active'
+//     }
+// })
+
+// var slideImages;
+// for (var i = 0; i < slideImages.length; i++) {
+//     console.log(slideImages[i]);
+// }
+
+// $('image2').hide();
+// $('image3').hide();
+
+$('#prevButton').click(function(){
+    console.log("previous button is clicked");
+})
+
+
+$('#nextButton').click(function(){
+    console.log("next button is clicked");
+})
